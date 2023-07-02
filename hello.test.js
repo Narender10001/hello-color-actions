5b82e63d5f14d22ef5178ccc295dc5cd375a4f14
Narender Chinthamu <narender.chinthamu@gmail.com>
2023-07-02 18:40:49 -0500

    changed Hello There to Hello There Narender

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hello There");
+    expect(hello.hello()).toEqual("Hello There Narender");
   });
 });
